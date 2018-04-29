1c771c4cfc553ebf38d8ee8c6c444b3c4217e70d
Luiz Carlos Rumbelsperger Viana <maxd13@devuan.i.zbook.local>
2018-04-29 15:31:28 -0300

    Middle of Part 3.

--- main.c
+++ main.c
@@ -2,6 +2,51 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
+
+const __uint32_t ID_SIZE = size_of_attribute(Row, id);
+const __uint32_t USERNAME_SIZE = size_of_attribute(Row, username);
+const __uint32_t EMAIL_SIZE = size_of_attribute(Row, email);
+const __uint32_t ID_OFFSET = 0;
+const __uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
+const __uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
+const __uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
+
+
+void serialize_row(Row* source, void* dest){
+  memcpy(dest+ID_OFFSET, &(source->id), ID_SIZE);
+  memcpy(dest+USERNAME_OFFSET, &(source->username), USERNAME_SIZE);
+  memcpy(dest+EMAIL_OFFSET, &(source->email), EMAIL_SIZE);
+}
+
+
+void deserialize_row(void* source, Row* dest){
+  memcpy(&(dest->id), source + ID_OFFSET, ID_SIZE);
+  memcpy(&(dest->username), source+USERNAME_OFFSET, USERNAME_SIZE);
+  memcpy(&(dest->email), source+EMAIL_OFFSET, EMAIL_SIZE);
+}
+
+const __uint32_t PAGE_SIZE = 4096;
+const __uint32_t TABLE_MAX_PAGES = 100;
+const __uint32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
+const __uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
+
+struct Table_t {
+  void* pages[TABLE_MAX_PAGES];
+  __uint32_t num_rows;
+};
+typedef struct Table_t Table;
+
+void* row_slot(Table* table, __uint32_t row_num){
+  __uint32_t page_num = row_num / ROWS_PER_PAGE;
+  void* page = table->pages[page_num];
+  // alloc memory in case there is no page.
+  if(!page) page = table->pages[page_num] = malloc(PAGE_SIZE);
+  __uint32_t row_offset = row_num % ROWS_PER_PAGE;
+  __uint32_t byte_offset = row_offset * ROW_SIZE;
+  return page + byte_offset;
+}
+
 struct InputBuffer_t {
   char* buffer;
   size_t buffer_length;
@@ -40,7 +85,7 @@ enum MetaCommandResult_t {
 };
 typedef enum MetaCommandResult_t MetaCommandResult;
 
-enum PrepareResult_t { PREPARE_SUCCESS, PREPARE_UNRECOGNIZED_COMMAND};
+enum PrepareResult_t { PREPARE_SUCCESS, PREPARE_UNRECOGNIZED_COMMAND, PREPARE_SYNTAX_ERROR};
 typedef enum PrepareResult_t PrepareResult;
 
 
@@ -55,14 +100,29 @@ MetaCommandResult do_meta_command(InputBuffer* input_buffer) {
 enum StatementType_t { STATEMENT_INSERT, STATEMENT_SELECT };
 typedef enum StatementType_t StatementType;
 
+const __uint32_t COLUMN_USERNAME_SIZE = 32;
+const __uint32_t COLUMN_EMAIL_SIZE = 255;
+
+struct Row_t {
+  __uint32_t id;
+  char username[COLUMN_USERNAME_SIZE];
+  char email[COLUMN_EMAIL_SIZE];
+};
+
+typedef struct Row_t Row;
+
 struct Statement_t {
   StatementType type;
+  Row row_to_insert;
 };
 typedef struct Statement_t Statement;
 
 PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement){
   if(strncmp(input_buffer->buffer, "insert", 6) == 0){
     statement->type = STATEMENT_INSERT;
+    int num_args = sscanf(input_buffer->buffer, "insert %d %s %s", 
+      &(statement->row_to_insert.id), statement->row_to_insert.username, statement->row_to_insert.email);
+    if(num_args < 3) return PREPARE_SYNTAX_ERROR;
     return PREPARE_SUCCESS;
   }
   
